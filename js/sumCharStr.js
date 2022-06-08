b6492bd227d966ed8e037e7f4fdb2e4a00be78a5
jheart-vic <adebowalevictorjheart.com>
2022-06-08 09:40:28 +0100

    Solve the testing exercise

--- js/sumCharStr.js
+++ js/sumCharStr.js
@@ -1,10 +1,5 @@
 function stringLength(string) {
   let stringlen = string.length;
-  // if(stringlen >= 1  && stringlen < 10){
-  //   return "good job"
-  // } else if(stringlen < 1  && stringlen > 10)  {
-  //   return "stringlength must be at least 1 and less than 10"
-  // }
   return stringlen
 }
   module.exports = stringLength;
